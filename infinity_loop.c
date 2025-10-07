8393e423d0510e7bedeb7fd5bf0b7f8286ea896a
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-10-07 23:08:17 +0600

    infinity loop

---
+++ infinity_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
+// ctrl + c diye terminal stop korbo
+// output.txt file ta delete kore notun kore abar khulte hobe
