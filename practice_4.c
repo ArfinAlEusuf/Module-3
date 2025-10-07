eb6d4555707d5f216556f9c8251e2e9e6299987c
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-10-08 00:08:11 +0600

    practice_4

---
+++ practice_4.c
@@ -0,0 +1,7 @@
+// Write the differences between break and continue
+
+// break:
+//     loop er moddhe jodi break thake tahole loop puropuri bondho hoye jabe. ar onno kono implimentation o hobe na condition o cheak hobe na. loop khalash hoye jabe.
+
+// continue:
+//     loop er moddhe jodi continue thake tahole sudhu oi condition er jonno kono kaj thakle ta print hobe na.orthat continue er porer line ar exicute hobe na. er por theke abar implimentation kore condition cheak kore loop colte thakbe shavabik vhabe.
